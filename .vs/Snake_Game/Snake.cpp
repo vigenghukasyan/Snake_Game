4bccf3cf27a1bb1478e5a47a8bc9c25a56416ff5
ᴠɪɢᴇɴ ɢʜᴜᴋᴀꜱʏᴀɴ <vigghukasyan@gmail.com>
2024-06-16 13:36:25 +0400

    Snake.cpp
    Added head, modes of movement and function to eating the fruit

--- .vs/Snake_Game/Snake.cpp
+++ .vs/Snake_Game/Snake.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 #include <Windows.h>
+#include <conio.h>
+#include <ctime>
 using namespace std;
 
 class Snake {
@@ -12,6 +14,7 @@ private:
 public:
 
     Snake() {
+        srand(time(0));
         gameOver = false;
         x = width / 2;
         y = height / 2;
@@ -35,7 +38,12 @@ public:
             for (int j = 0; j < width; j++) {
                 if (j == 0)
                     cout << "#"; // Left wall
-                cout << " "; // Empty space
+                if (i == y && j == x)
+                    cout << "O";
+                else if (i == fruitY && j == fruitX)
+                    cout << "F";
+                else
+                    cout << " "; // Empty space
 
                 if (j == width - 1)
                     cout << "#"; // Right wall
@@ -54,11 +62,51 @@ public:
 
 
     void Input() {
-
+        if (_kbhit()) {
+    switch (_getch()) {
+    case 'w':
+        dir = UP;
+        break;
+    case 'a':
+        dir = LEFT;
+        break;
+    case 'd':
+        dir = RIGHT;
+        break;
+    case 's':
+        dir = DOWN;
+        break;
+    case 'x':
+        gameOver = true;
+        break;
+    }
+        }
     }
 
     void Logic() {
-
+        switch (dir) {
+        case LEFT:
+            x--;
+            break;
+        case RIGHT:
+            x++;
+            break;
+        case UP:
+            y--;
+            break;
+        case DOWN:
+            y++;
+            break;
+        default:
+            break;
+        }
+        if (x > width || x < 0 || y > height || y < 0)
+            gameOver = true;
+        if (x == fruitX && y == fruitY) {
+            score += 10;
+            fruitX = rand() % width;
+            fruitY = rand() % height;
+        }
     }
 
 };
@@ -69,6 +117,7 @@ int main() {
         snake.Draw();
         snake.Input();
         snake.Logic();
+        Sleep(100);
     }
 
 
